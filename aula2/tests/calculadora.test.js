49d229b11872d541fe113b56d243f51d1f2b8f10
Tatiana Ciocca Almeida <18190041+Tacial@users.noreply.github.com>
2025-09-04 17:54:58 -0300

    Add files via upload
    aula2 - calculadora - base

---
+++ aula2/tests/calculadora.test.js
@@ -0,0 +1,11 @@
+const { somarDoisNumeros } = require('../src/calculadora');
+
+const resultadoDaSoma = somarDoisNumeros(5, 3);
+
+console.log(resultadoDaSoma)
+
+// git add src/
+// git add tests/
+// git status
+// git commit -m ""
+// git push origin main
